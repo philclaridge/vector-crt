b9f1cbf7934523fc34152da76805f13a0bbadd0c
Phil Claridge <>
2024-07-17 08:01:08 +0100

    Merged, but not integrated, Vite

--- src/script.ts
+++ src/script.ts
@@ -1,55 +1,68 @@
 // Copyright 2024, Philip Claridge
 // SPDX-License-Identifier: MIT
-import { generateCdcCheckTable, vectorCharacterRomCDC6602 } from './vectorRomCDC6602.js';
-import { applyFilters, convertRomToDrawnVectorSegments, drawVectorsUsingCanvas, repeatCRTData, simplifyLines } from "./lineDraw.js";
-import { IIRFilter } from "./filters.js";
-const bitmapCanvas = document.getElementById('bitmapCanvas');
-const bitmapCanvasContext = bitmapCanvas.getContext('2d', { willReadFrequently: true });
-const sizeSlider = document.getElementById('sizeSlider');
-const sizeValue = document.getElementById('sizeValue');
-const characterSelect = document.getElementById('characterSelect');
-const pixelsPerVectorStepSlider = document.getElementById('scaleFactorSlider');
-const pixelsPerVectorStepValue = document.getElementById('scaleFactorValue');
-const borderWidthSlider = document.getElementById('borderWidthSlider');
-const borderWidthValue = document.getElementById('borderWidthValue');
-const lineWidthSlider = document.getElementById('lineWidthSlider');
-const lineWidthValue = document.getElementById('lineWidthValue');
-const lineCapSelect = document.getElementById('lineCapSelect');
-const lineJoinSelect = document.getElementById('lineJoinSelect');
-const drawPixelReferenceCheckerboardToggle = document.getElementById('drawPixelReferenceCheckerboardToggle');
-const lineAlphaSelect = document.getElementById('lineAlphaSelect');
-const redrawButton = document.getElementById('redrawButton');
-const checkTableContainerContent = document.getElementById('checkTableContainerContent');
-const debugOutputContent = document.getElementById('debugOutputContent');
-let pixelsPerVectorStep;
-let strokeStyle;
-let lineWidthPixels;
-let borderWidthVectorSteps;
-let lineCap;
-let lineJoin;
-let displayZoomFactor;
-let drawPixelReferenceCheckerboard;
+
+import {generateCdcCheckTable, vectorCharacterRomCDC6602} from './vectorRomCDC6602.js';
+import {
+    applyFilters,
+    convertRomToDrawnVectorSegments, drawVectorsUsingCanvas,
+    repeatCRTData,
+    simplifyLines
+} from "./lineDraw";
+import {IIRFilter} from "./filters";
+
+const bitmapCanvas = document.getElementById('bitmapCanvas') as HTMLCanvasElement;
+const bitmapCanvasContext = bitmapCanvas.getContext('2d', {willReadFrequently: true}) as CanvasRenderingContext2D;
+const sizeSlider = document.getElementById('sizeSlider') as HTMLInputElement;
+const sizeValue = document.getElementById('sizeValue') as HTMLSpanElement;
+const characterSelect = document.getElementById('characterSelect') as HTMLSelectElement;
+const pixelsPerVectorStepSlider = document.getElementById('scaleFactorSlider') as HTMLInputElement;
+const pixelsPerVectorStepValue = document.getElementById('scaleFactorValue') as HTMLSpanElement;
+const borderWidthSlider = document.getElementById('borderWidthSlider') as HTMLInputElement;
+const borderWidthValue = document.getElementById('borderWidthValue') as HTMLSpanElement;
+const lineWidthSlider = document.getElementById('lineWidthSlider') as HTMLInputElement;
+const lineWidthValue = document.getElementById('lineWidthValue') as HTMLSpanElement;
+const lineCapSelect = document.getElementById('lineCapSelect') as HTMLSelectElement;
+const lineJoinSelect = document.getElementById('lineJoinSelect') as HTMLSelectElement;
+const drawPixelReferenceCheckerboardToggle = document.getElementById('drawPixelReferenceCheckerboardToggle') as HTMLInputElement;
+const lineAlphaSelect = document.getElementById('lineAlphaSelect') as HTMLSelectElement;
+const redrawButton = document.getElementById('redrawButton') as HTMLButtonElement;
+const checkTableContainerContent = document.getElementById('checkTableContainerContent') as HTMLDivElement;
+const debugOutputContent = document.getElementById('debugOutputContent') as HTMLDivElement;
+
+let pixelsPerVectorStep: number;
+let strokeStyle: string;
+let lineWidthPixels: number;
+let borderWidthVectorSteps: number;
+let lineCap: CanvasLineCap;
+let lineJoin: CanvasLineJoin
+let displayZoomFactor: number;
+let drawPixelReferenceCheckerboard: boolean;
+
 let canvasSizeVectorDots = 7;
-function readAllControls() {
+
+function readAllControls(): void {
     pixelsPerVectorStep = parseFloat(pixelsPerVectorStepSlider.value);
     strokeStyle = `rgba(0, 0, 0, ${lineAlphaSelect.value})`;
     lineWidthPixels = parseFloat(lineWidthSlider.value);
     borderWidthVectorSteps = parseFloat(borderWidthSlider.value);
-    lineCap = lineCapSelect.value;
-    lineJoin = lineJoinSelect.value;
+    lineCap = lineCapSelect.value as CanvasLineCap;
+    lineJoin = lineJoinSelect.value as CanvasLineJoin;
     drawPixelReferenceCheckerboard = drawPixelReferenceCheckerboardToggle.checked;
     displayZoomFactor = parseInt(sizeSlider.value);
+
     pixelsPerVectorStepValue.textContent = `${pixelsPerVectorStepSlider.value} px`;
     lineWidthValue.textContent = `${lineWidthSlider.value} px`;
     borderWidthValue.textContent = `${borderWidthSlider.value} step`;
     sizeValue.textContent = `${displayZoomFactor}x`;
 }
-function updateCanvasSize() {
+
+function updateCanvasSize(): void {
     const canvasSize = (canvasSizeVectorDots * pixelsPerVectorStep) + (pixelsPerVectorStep * borderWidthVectorSteps * 2);
     bitmapCanvas.width = canvasSize;
     bitmapCanvas.height = canvasSize;
 }
-function drawCheckerboard() {
+
+function drawCheckerboard(): void {
     const canvasWidth = bitmapCanvas.width;
     const canvasHeight = bitmapCanvas.height;
     for (let y = 0; y < canvasHeight; y++) {
@@ -59,27 +72,52 @@ function drawCheckerboard() {
         }
     }
 }
-function drawCharacter() {
+
+function drawCharacter(): void {
     const character = characterSelect.value;
+
     const characterFromVectorCharacterRomCDC6602 = vectorCharacterRomCDC6602[character];
+
     const segmentsFromRom = convertRomToDrawnVectorSegments(characterFromVectorCharacterRomCDC6602);
+
     const simplifiedLineSegments = simplifyLines(segmentsFromRom);
+
     const repeatedData = repeatCRTData(characterFromVectorCharacterRomCDC6602, 10);
-    const iirFilteredData = applyFilters(repeatedData, new IIRFilter(0.08), new IIRFilter(0.08), new IIRFilter(0.1));
-    drawVectorsUsingCanvas(bitmapCanvasContext, pixelsPerVectorStep * borderWidthVectorSteps, (canvasSizeVectorDots * pixelsPerVectorStep) + (pixelsPerVectorStep * borderWidthVectorSteps), strokeStyle, lineWidthPixels, lineCap, lineJoin, segmentsFromRom, pixelsPerVectorStep);
+    const iirFilteredData = applyFilters(
+        repeatedData,
+        new IIRFilter(0.08),
+        new IIRFilter(0.08),
+        new IIRFilter(0.1)
+    );
+
+    drawVectorsUsingCanvas(
+        bitmapCanvasContext,
+        pixelsPerVectorStep * borderWidthVectorSteps,
+        (canvasSizeVectorDots * pixelsPerVectorStep) + (pixelsPerVectorStep * borderWidthVectorSteps),
+        strokeStyle,
+        lineWidthPixels,
+        lineCap,
+        lineJoin,
+        segmentsFromRom,
+        pixelsPerVectorStep);
+
     debugOutputContent.innerHTML = `<p>Character: ${character}</p>`;
     debugOutputContent.innerHTML += `<p>Vector Drawn Segments From Rom: ${JSON.stringify(segmentsFromRom)}</p>`;
     debugOutputContent.innerHTML += `<p>Vector Drawn Simplified Segments: ${JSON.stringify(simplifiedLineSegments)}</p>`;
+
     debugOutputContent.innerHTML += `<p>Raw Char Gen Table: ${JSON.stringify(characterFromVectorCharacterRomCDC6602)}</p>`;
     debugOutputContent.innerHTML += `<p>Repeated Data: ${JSON.stringify(repeatedData)}</p>`;
     debugOutputContent.innerHTML += `<p>IIR Filtered: ${JSON.stringify(iirFilteredData)}</p>`;
+
     checkTableContainerContent.innerHTML = generateCdcCheckTable(characterFromVectorCharacterRomCDC6602);
 }
-function updateZoom() {
+
+function updateZoom(): void {
     bitmapCanvas.style.width = `${bitmapCanvas.width * displayZoomFactor}px`;
     bitmapCanvas.style.height = `${bitmapCanvas.height * displayZoomFactor}px`;
 }
-function updateDisplay() {
+
+function updateDisplay(): void {
     readAllControls();
     updateCanvasSize();
     bitmapCanvasContext.clearRect(0, 0, bitmapCanvas.width, bitmapCanvas.height);
@@ -89,7 +127,8 @@ function updateDisplay() {
     drawCharacter();
     updateZoom();
 }
-function initialise() {
+
+function initialise(): void {
     sizeSlider.addEventListener('input', updateDisplay);
     pixelsPerVectorStepSlider.addEventListener('input', updateDisplay);
     redrawButton.addEventListener('click', updateDisplay);
@@ -103,5 +142,7 @@ function initialise() {
     // Initial draw
     updateDisplay();
 }
+
 initialise();
-//# sourceMappingURL=script.js.map
+
+
